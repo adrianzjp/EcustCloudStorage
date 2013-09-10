600ff690e04819ab240adf045dfb551928a19aae
adrianzjp <zhoujip@yeah.net>
2013-09-10 11:17:46 +0800

    archetiture0

---
+++ api/settings.py
@@ -0,0 +1,7 @@
+# Django settings for newproject project.
+
+HOST_IP = '172.20.46.11'
+
+AUTH_URL = 'http://172.20.46.11:5000/v2.0/'
+#'http://172.20.46.11:5000/v2.0/'#'
+MANAGEMENT_URL ='http://172.20.46.11:35357/v2.0'
